19d18d11b5131d83330af2908680229122c29345
Mohid Bin Zubair <mohidbinzubair792@gmail.com>
2026-02-13 20:12:42 +0500

    util code discussion

---
+++ src/app.js
@@ -0,0 +1,15 @@
+import express from "express";
+import coer from "cors"
+import cookieParser from "cookie-parser";
+const app = express
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN
+}))
+
+app.use(express.json({ limit:"16kb" }))
+app.use(express.urlencoded())
+app.use(express.static("public"))
+app.use(cookieParser())
+
+export {app}
